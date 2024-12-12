57faa6ab5a4be0b3d227f11ba6254f4ae10ef2d5
Daniel <Daniel2003@gmail.com>
2024-12-12 02:18:06 -0300

    Portifolio do chef

---
+++ src/js/Top-line.js
@@ -0,0 +1,12 @@
+const topLine = document.querySelector('.top-line');
+
+const HtmlTopLine = 
+`
+    <div class="intro-name">
+        <h1>Meu nome é Chefe! <img src="src/img/mao.gif" alt=""></h1>
+        <h2>Controle de Rataria </h2>
+    </div>
+    <img src="https://i.pinimg.com/736x/ea/0d/ba/ea0dba57c67e704d87d7bdf6cc406f05.jpg" alt="photo-portfolio" class="photo" width="150" height="150">
+`;
+
+topLine.innerHTML = HtmlTopLine;
---
+++ src/js/contact.js
@@ -0,0 +1,24 @@
+const Mecontate = document.querySelector('.contat');
+
+const Social =
+ `
+             <div class="social">
+                <a href="">
+                    <img src="src/img/linkedin.svg" alt="linkedin-svg">
+                </a>
+                <a href="">
+                    <img src="src/img/whatsapp.svg" alt="linkedin-svg">
+                </a>
+                <a href="">
+                    <img src="src/img/instagram.svg" alt="linkedin-svg">
+                </a>
+            </div>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=chefe.gato@gmail.com&su=Possível%20vaga%20na%20área%20de%20Controle%20de%20Rataria&body=Eu%20gostaria%20de%20discutir%20sobre%20a%20vaga%20de%20Supervisor%20Felino.%0A%0AAguardo%20seu%20retorno.%0A%0AAtenciosamente,
+            ">
+                <button>
+                    Mia pra mim 🐾 
+                </button>
+            </a>
+ `;
+
+ Mecontate.innerHTML = Social;
